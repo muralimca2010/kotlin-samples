066d8365c78b16e604ba2cd8e08c8e512bee6dee
muralidharanmohan <murali.mca2010@gmail.com>
2020-10-28 14:35:31 +0530

    DataClass samples.

---
+++ src/com/kotlin/classes/SampleDataClass.kt
@@ -0,0 +1,68 @@
+
+data class UserData(val uName: String, var uAge: Int)
+
+fun main() {
+    val velan = UserData("Velan", 1)
+    println("User Name : ${velan.uName}")
+    println("User Age : ${velan.uAge}")
+
+    //Copy
+    val copyVelan = velan.copy()
+
+    println("Copied Object : ${copyVelan.uName}")
+
+    //Copy with Change value
+    val copyObjectValueChange = velan.copy("Kumaran", 6)
+    println("UserName: ${copyObjectValueChange.uName} , UserAge: ${copyObjectValueChange.uAge}")
+
+
+    // hashCode ==
+    if (velan.uName.hashCode() == copyVelan.uName.hashCode()) {
+        println(velan.uName.hashCode())
+        println(copyVelan.uName.hashCode())
+        println("Both same")
+    } else {
+        println("Not same")
+    }
+
+    // hashCode equals()
+    if (velan.uName.hashCode().equals(copyObjectValueChange.uName.hashCode())) {
+        println(velan.uName.hashCode())
+        println(copyObjectValueChange.uName.hashCode())
+        println("Both same")
+    } else {
+        println(velan.uName.hashCode())
+        println(copyObjectValueChange.uName.hashCode())
+        println("Not same")
+    }
+
+
+    val velantoString = velan.copy(velan.uAge.toString(), 12)
+    println("UserName: ${velantoString.uName}, UserAge: ${velantoString.uAge}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Kotlin Data Class Requirements
+Here are the requirements:
+
+The primary constructor must have at least one parameter.
+The parameters of the primary constructor must be marked as either val (read-only) or var (read-write).
+The class cannot be open, abstract, inner or sealed.
+The class may extend other classes or implement interfaces. If you are using Kotlin version before 1.1, the class can only implement interfaces.*/
+
+/*
+For this class, the compiler automatically generates:
+
+copy() function, equals() and hashCode() pair, and toString() form of the primary constructor
+componentN() functions*/
